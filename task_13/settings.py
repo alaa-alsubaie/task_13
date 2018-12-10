1db1f493a21cf1e5d4a91cb0a9799a062064b079
“AlaaAlsubaie” <eng_a.a.a@hotmail.com>
2018-12-10 19:18:54 +0300

    task13_server settings

--- task_13/settings.py
+++ task_13/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','68.183.209.34']
 
 
 # Application definition
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'task_13.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+      #'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '405a599b27b208d887e82c435553adbf',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
